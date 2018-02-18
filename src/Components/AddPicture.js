bb27caac0097f4fa1358f0477df0b31f63677766
ryanbrown358 <ryanbrown358@gmail.com>
2018-02-18 13:33:00 -0700

    added

--- src/Components/AddPicture.js
+++ src/Components/AddPicture.js
@@ -1,22 +1,32 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 
 
-export default class AddPicture extends Component{
+export default class NavBar extends Component{
     render(){
         return(
+
             <div>
-                <form action="POST">
-                <div className="file-field input-field">
-                <div className="btn">
-                <span>File</span>
-                <input type="file" multiple/>
-                </div>
-                 <div className="file-path-wrapper">
-                <input className="file-path validate" type="text" placeholder="Upload one or more files"/>
-                </div>
-                </div>
-            </form>
+                  <nav>
+                    <div className="nav-wrapper  amber accent-4">
+                    <div className="nav-wrapper right">
+                        <form>
+                            <div className="input-field">
+                            <input id="search" type="search" required />
+                            <label className="label-icon" htmlFor="search"><i className="material-icons">search</i></label>
+                            </div>
+                        </form>
+                        </div>
+                    <ul id="nav-mobile" className="left hide-on-med-and-down">
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/UploadPictures">Upload Items</Link></li>
+                        <li><Link to="/Items" >Items For Sale</Link></li>
+                        <li><a >JavaScript</a></li>
+                    </ul>
+                  
+                    </div>
+                </nav>
             </div>
         );
     }
