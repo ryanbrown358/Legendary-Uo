09879eedbe37b6bf32fdf46b4d9944a987222528
ryanbrown358 <ryanbrown358@gmail.com>
2018-03-04 13:25:00 -0700

    added a temp placeholder for picture

--- src/Components/ViewItems.js
+++ src/Components/ViewItems.js
@@ -3,22 +3,50 @@ import React, { Component } from 'react';
 
 export default class ViewItems extends Component{
     render(){
+        let card ={
+            padding: '10px'
+        }
         return(
             <div>
-                    <div>
-                        <div className="card">
-                            <div className="card-image waves-effect waves-block waves-light">
-                            <img className="activator" alt="img"/>
+                         <div className="row" style={card}>
+                            <div className="col s1 m7 l2" >
+                            <div className="card" >
+                                <div className="card-image">
+                                <img src="https://i.imgur.com/fglN8mc.png"/>
+                                <span className="card-title" stlye={card}></span>
+                                </div>
+                                <div className="card-content">
+                                <p>Discription goes here</p>
+                                <button type="button" value="submit">Buy</button>
+                                </div>
+                                <div className="card-action">
+                                {/* <a href="#">This is a link</a> */}
+                                </div>
+
                             </div>
-                            <div className="card-content">
-                            <span className="card-title activator grey-text text-darken-4">Card Title<i className="material-icons right">more_vert</i></span>
-                            <p><a>This is a link</a></p>
                             </div>
-                            <div className="card-reveal">
-                            <span className="card-title grey-text text-darken-4">Card Title<i className="material-icons right">close</i></span>
-                            <p>Here is some more information about this product that is only revealed once clicked on.</p>
+                            
+
+                            <div className="row" style={card}>
+                            <div className="col s1 m7 l2" >
+                            <div className="card" >
+                                <div className="card-image">
+                                <img src="https://i.imgur.com/fglN8mc.png"/>
+                                <span className="card-title" stlye={card}></span>
+                                </div>
+                                <div className="card-content">
+                                <p>Discription goes here</p>
+                                <button type="button" value="submit">Buy</button>
+                                </div>
+                                <div className="card-action">
+                                {/* <a href="#">This is a link</a> */}
+                                </div>
+
+                            </div>
+                            </div>
+                           
                         </div>
-                    </div>
+
                 </div>
             </div>
         );
